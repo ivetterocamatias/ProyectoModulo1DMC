363a08c7a91d343dbabd648fa736c23e19f840ac
ivetterocamatias <ivetterocamatias@gmail.com>
2026-07-26 00:08:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title("Proyecto Módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
-valor final = st.number_input("Ingrese el valor final", value = 0)
+valor final = st.number_input("Ingrese el valor final", value = 1)
 
 lista_numerica = list(range(valor_inicial,valor_final))
 
