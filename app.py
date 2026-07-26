dd33eb9ffe07f65d7b5eeb16f5e83a160e6001e7
ivetterocamatias <ivetterocamatias@gmail.com>
2026-07-26 02:46:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.title("Proyecto Módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 st.image("Python_logo.png")
+st.sidebar.image("DMC.png")
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
 valor_final = st.number_input("Ingrese el valor final", value = 1)
